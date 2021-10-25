0f03adcd3fa8cf79d7bc3b580c31ce918bf5e2cb
unknown <vee.chitato1@gmailcom>
2021-10-25 15:48:36 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuan4_recycler/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.pertemuan4_recycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addData();
+        recyclerView =
+                (RecyclerView)findViewById(R.id.recycleview);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Vee", "1918036", "2019"));
+        mahasiswaArrayList.add(new Mahasiswa("Pio", "1918042", "2019"));
+        mahasiswaArrayList.add(new Mahasiswa("Alang", "1711103", "2017"));
+    }
+}
